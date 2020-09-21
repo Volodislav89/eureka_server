1531618613ac81fabce0f6e4bd63a8b07ec8af9e
“Volodislav89” <shbloodislav@gmail.com>
2020-09-21 13:00:35 +0300

    properties change

--- src/main/java/com/spring_cloud/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/spring_cloud/eureka_server/EurekaServerApplication.java
@@ -1,15 +1,17 @@
 package com.spring_cloud.eureka_server;
 
+import com.google.inject.internal.asm.$ClassWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.concurrent.Callable;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
-
+   
 }
